3bd1c21cbe3acf5fab5ecc23de5d27ae8fda0fca
Temesgen Berhanu <temesgenberhanu674@gmail.com>
2026-03-11 00:44:51 -0700

    implement active and personal listing query endpoints

--- src/routes/listing.routes.js
+++ src/routes/listing.routes.js
@@ -1,11 +1,22 @@
 import express from "express";
 const router = express.Router();
 import { protect, restrictTo } from "../middlewares/auth.middleware.js";
-import { createListing } from "../controllers/listing.controller.js";
+import {
+    createListing,
+    getActiveListings,
+    getMyListings,
+    getListingById,
+} from "../controllers/listing.controller.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 router.post("/", protect, restrictTo("farmer"), asyncHandler(createListing));
-
-// Later: GET /my-listings, GET /active, etc.
+router.get("/active", protect, asyncHandler(getActiveListings));
+router.get(
+    "/my-listings",
+    protect,
+    restrictTo("farmer"),
+    asyncHandler(getMyListings),
+);
+router.get("/:id", protect, asyncHandler(getListingById));
 
 export default router;
